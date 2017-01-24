0b3866e0c66da5800722e26c59a1e03d10c4e312
Simon <simonxd@gmail.com>
2017-01-24 15:55:47 -0200

    exit process upon rabbit mq connection close

--- cli/start.js
+++ cli/start.js
@@ -24,18 +24,28 @@ var options = envOptions({
 // instantiate the app
 var app = hWebsiteServer(options);
 
+// create http server and pass express app as callback
+var server = http.createServer(app);
+
 app.ready.then(() => {
   console.log('h-website-server setup ready');
+
+  // start listening
+  server.listen(options.port, function () {
+    console.log('h-website-server listening at port %s', options.port);
+  });
+
+  /**
+   * Exit process upon rabbit mq connection close.
+   * Let environment deal with reconnection.
+   */
+  app.services.hWebsiteEventsConsumer.on('channel-close', (e) => {
+    console.warn('h-website-server hWebsiteEventsConsumer channel-close', e);
+    process.exit(1);
+  });
+
 })
 .catch((err) => {
   console.warn('h-website-server setup error', err);
   process.exit(1);
-});
-
-// create http server and pass express app as callback
-var server = http.createServer(app);
-
-// start listening
-server.listen(options.port, function () {
-  console.log('h-website-server listening at port %s', options.port);
 });
